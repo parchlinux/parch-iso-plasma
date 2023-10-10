4359e2ade34d044a018ca8dc6dcaf4952bf6658c
Sohrab Behdani <behdanisohrab@gmail.com>
2023-10-10 14:33:52 +0330

    updated the upload script

--- tools/upload_asset.py
+++ tools/upload_asset.py
@@ -9,7 +9,7 @@ from github import Github
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
-api_key = os.getenv("api_key", None)
+api_key = os.getenv("API_KEY", None)
 current_date = datetime.today().strftime("%Y-%m-%d")
 
 
@@ -28,29 +28,29 @@ file_name = _path.name
 
 hash = _compute_sha256(path)
 
-repo_name = os.getenv("name", None)
-release_name = os.getenv("release_name", None)
+repo_name = os.getenv("REPO_NAME", None)
+release_name = os.getenv("RELEASE_NAME", None)
 
 logging.info("Starting at %s", current_date)
 
 if not (repo_name and api_key and release_name):
     logging.error(
-        "'repo_name'/'api_key'/'release_name' not found in your envs."
-        "please add this and run again"
+        "'REPO_NAME'/'API_KEY'/'RELEASE_NAME' not found in your environment vars."
+        "please add this and run again."
     )
     sys.exit(1)
 
 
 gh = Github(api_key)
 print(repo_name, api_key)
-repo = gh.get_repo(f"parchlinux/{repo_name}")
+repo = gh.get_repo(repo_name)
 
 release = repo.get_release(release_name)
 
-logging.info("statrting Upload ISO to release")
+logging.info("Statrting upload ISO to release")
 
 release.upload_asset(path=path)
-logging.info("ISO upload is done")
+logging.info("ISO uploaded.")
 
 # update release
 msg = (
@@ -60,6 +60,6 @@ msg = (
 | :---: | :---: |
 | {file_name} | {hash} |"""
 )
-logging.info("Starting Update release msg with: \n %s" % msg)
+logging.info("Starting update release msg with: \n %s" % msg)
 release.update_release(name=release.tag_name, message=msg)
-logging.info("Release Update is done.")
+logging.info("Release update is done.")
