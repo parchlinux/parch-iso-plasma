dc04f7ab06e134c39981ef963af2bbf0e1ed1692
Sohrab Behdani <behdanisohrab@gmail.com>
2024-04-06 22:57:00 +0330

    disabled grub

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -3,14 +3,14 @@
 
 iso_name="Parchlinux Plasma"
 iso_label="PARCHLINUX_PLASMA_$(date +%Y%m)"
-iso_publisher="Parch Linux <parchlinux.ir>"
+iso_publisher="Parch Linux <parchlinux.com>"
 iso_application="Parch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-	    'uefi-x64.systemd-boot.esp'
-	    'uefi-x64.systemd-boot.eltorito')
+	   'uefi-x64.systemd-boot.esp'
+      'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
